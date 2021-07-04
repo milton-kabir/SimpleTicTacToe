4413864ce966aecae34dd0d60137faf7ae31dbc8
Milton Kabir <drifter.kabir@gmail.com>
2021-07-04 18:52:07 +0600

    First Stage completed

---
+++ src/com/kabir/milton/Main.java
@@ -0,0 +1,11 @@
+package com.kabir.milton;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O");
+    }
+}
