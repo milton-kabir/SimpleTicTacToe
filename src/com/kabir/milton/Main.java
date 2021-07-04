60729751865f4fa63bb554427b4793bb7a1a4055
Milton Kabir <drifter.kabir@gmail.com>
2021-07-04 19:00:00 +0600

    Second Stage completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,11 +1,19 @@
 package com.kabir.milton;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O");
+        System.out.println("Enter cells:");
+        Scanner sc=new Scanner(System.in);
+        String st=sc.nextLine();
+        System.out.println("---------");
+        System.out.println("| "+st.charAt(0)+" "+st.charAt(1)+" "+st.charAt(2)+" |");
+        System.out.println("| "+st.charAt(3)+" "+st.charAt(4)+" "+st.charAt(5)+" |");
+        System.out.println("| "+st.charAt(6)+" "+st.charAt(7)+" "+st.charAt(8)+" |");
+
+        System.out.println("---------");
     }
 }
